665ff36d145090f7c02c883aa9ecf99efc147196
VitalyBordyashov <vital.bordyashov@mail.ru>
2022-03-30 23:19:17 +0300

    записал функцию решения квадратного уравнения

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,20 @@
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let discriminant;
+  arr = [];
+  discriminant = Math.pow(b, 2) - 4*a*c;
+  if (discriminant < 0) {
+    arr = [];
+  } else if (discriminant == 0) {
+    let x = -b/(2*a)
+    arr.push(x);
+  } else {
+    let x1 = (-b - (Math.pow(discriminant, -2)))/(2*a);
+    let x2 = (-b + (Math.pow(discriminant, -2)))/(2*a);
+    arr.push(x1);
+    arr.push(x2);
+  }
+  return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
