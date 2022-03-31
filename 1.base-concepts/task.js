b8f31af63cec638e7d8af4c8fd5090b9c40992cf
Vitaly Bordyashov <vital.bordyashov>
2022-03-31 13:51:39 +0300

    записал функцию вычисления кредита

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -19,7 +19,11 @@ function solveEquation(a, b, c) {
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
-
+  let today = new Date();
+  let n = (date - today) / (30,5 * 60 * 60 * 24 * 1000);
+  let s = amount - contribution;
+  let p = 1/12 * percent;
+  totalAmount = s * (p + (p / ( Math.pow((1 + p), n) - 1)));
   // код для задачи №2 писать здесь
 
   return totalAmount;
