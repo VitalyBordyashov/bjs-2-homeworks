8cdd1f8933e82412e236cbd019dbec2a7a5bdb2a
Vitaly Bordyashov <vital.bordyashov>
2022-04-01 11:35:14 +0300

    исправил ошибки в коде

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,3 +1,5 @@
+"use strict";
+
 function solveEquation(a, b, c) {
   let arr;
   let discriminant;
@@ -5,12 +7,12 @@ function solveEquation(a, b, c) {
   discriminant = Math.pow(b, 2) - 4*a*c;
   if (discriminant < 0) {
     arr = [];
-  } else if (discriminant == 0) {
-    let x = -b/(2*a)
+  } else if (discriminant === 0) {
+    let x = -b / (2 * a)
     arr.push(x);
   } else {
-    let x1 = (-b - (Math.pow(discriminant, -2)))/(2*a);
-    let x2 = (-b + (Math.pow(discriminant, -2)))/(2*a);
+    let x1 = (-b + (Math.sqrt(discriminant)))/(2*a);
+    let x2 = (-b - (Math.sqrt(discriminant)))/(2*a);
     arr.push(x1);
     arr.push(x2);
   }
@@ -19,12 +21,17 @@ function solveEquation(a, b, c) {
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
+  if (isNaN(parseInt(percent))) {
+    return ('Параметр "Процентная ставка" содержит неправильное значение ' + percent);
+  } else if (isNaN(parseInt(contribution))) {
+    return ('Параметр "Первоначальный взнос" содержит неправильное значение ' + contribution);
+  } else if (isNaN(parseInt(amount))) {
+    return ('Параметр "Общая стоимость" содержит неправильное значение ' + amount);
+  }
   let today = new Date();
-  let n = (date - today) / (30,5 * 60 * 60 * 24 * 1000);
+  let n = Math.round((date - today) / (30.5 * 60 * 60 * 24 * 1000));
   let s = amount - contribution;
-  let p = 1/12 * percent;
-  totalAmount = s * (p + (p / ( Math.pow((1 + p), n) - 1)));
-  // код для задачи №2 писать здесь
-
+  let p = 1/1200 * percent;
+  totalAmount = (n * (s * (p + (p / (Math.pow((1 + p), n) - 1))))).toFixed(2);
   return totalAmount;
 }
