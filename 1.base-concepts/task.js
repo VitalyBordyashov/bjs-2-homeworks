3c22fefbc75c3989a63cb598a65322571ed86a79
VitalyBordyashov <vital.bordyashov@mail.ru>
2022-04-01 20:19:07 +0300

    исправил ошибки после проверки

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -5,16 +5,12 @@ function solveEquation(a, b, c) {
   let discriminant;
   arr = [];
   discriminant = Math.pow(b, 2) - 4*a*c;
-  if (discriminant < 0) {
-    arr = [];
-  } else if (discriminant === 0) {
+    if (discriminant === 0) {
     let x = -b / (2 * a)
     arr.push(x);
-  } else {
-    let x1 = (-b + (Math.sqrt(discriminant)))/(2*a);
-    let x2 = (-b - (Math.sqrt(discriminant)))/(2*a);
-    arr.push(x1);
-    arr.push(x2);
+  } else if (discriminant > 0) {
+    arr.push((-b + (Math.sqrt(discriminant)))/(2*a));
+    arr.push((-b - (Math.sqrt(discriminant)))/(2*a));
   }
   return arr;
 }
@@ -22,16 +18,16 @@ function solveEquation(a, b, c) {
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
   if (isNaN(parseInt(percent))) {
-    return ('Параметр "Процентная ставка" содержит неправильное значение ' + percent);
+    return ("Параметр <Процентная ставка> содержит неправильное значение <" + percent + ">");
   } else if (isNaN(parseInt(contribution))) {
-    return ('Параметр "Первоначальный взнос" содержит неправильное значение ' + contribution);
+    return ("Параметр <Начальный взнос> содержит неправильное значение <" + contribution + ">");
   } else if (isNaN(parseInt(amount))) {
-    return ('Параметр "Общая стоимость" содержит неправильное значение ' + amount);
+    return ("Параметр <Общая стоимость> содержит неправильное значение <" + amount + ">");
   }
   let today = new Date();
   let n = Math.round((date - today) / (30.5 * 60 * 60 * 24 * 1000));
   let s = amount - contribution;
   let p = 1/1200 * percent;
-  totalAmount = (n * (s * (p + (p / (Math.pow((1 + p), n) - 1))))).toFixed(2);
+  totalAmount = Number((n * (s * (p + (p / (Math.pow((1 + p), n) - 1))))).toFixed(2));
   return totalAmount;
 }
