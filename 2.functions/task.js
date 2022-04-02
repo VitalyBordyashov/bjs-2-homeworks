b63ee4611604cf7cc8c6a761784a6b06231fd488
VitalyBordyashov <vital.bordyashov@mail.ru>
2022-04-02 09:56:37 +0300

    1 варинат

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,49 @@
 // Задание 1
 function getArrayParams(arr) {
   let min, max, sum, avg;
-
-  // Ваш код
-
+  min = arr[0];
+  max = min;
+  sum = max;
+  for (let i = 1; i < arr.length; i++) {
+    sum = sum + arr[i];
+    if (arr[i] < min) {
+      min = arr[i];
+    } else if (arr[i] > max) {
+      max = arr[i]
+    };
+  }
+  avg = Number((sum/arr.length).toFixed(2));
+  
   return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
   let sum;
-
-  // Ваш код
-
+  sum = arr[0];;
+  for (let i = 1; i < arr.length; i++) {
+    sum = sum + arr[i];
+  }
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
   let max;
-
-  // Ваш кода
-  // for ...
+  max = worker(arrOfArr[0]);
+  for (let i = 1; i < arrOfArr.length; i++) {
+    let y = worker(arrOfArr[i]);
+    if (y > max) {
+      max = y;
+    }
+  }  
   
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  getArrayParams(arr);
+  let differenceAmounts = Math.abs(max - min);
+  return differenceAmounts;
 }
+makeWork(arrOfArr, worker2);
