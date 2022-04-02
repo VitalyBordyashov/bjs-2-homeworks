9ce7bd57954737cc6e717ed31040558c6e0dccdd
VitalyBordyashov <vital.bordyashov@mail.ru>
2022-04-02 19:24:16 +0300

    решение

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,22 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
-
+  if (arr1.length != arr2.length) {
+    result = false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] === arr2[i]) {
+        result = true;
+      } else {
+        result = false;
+        break;
+      }
+    }
+  }
   return result; // boolean
 }
 
 function advancedFilter(arr) {
   let resultArr;
-
-  // Ваш код
-
+  resultArr  = arr.filter(elem => elem > 0).filter(elem => elem % 3 == 0).map(elem => elem * 10);
   return resultArr; // array
 }
