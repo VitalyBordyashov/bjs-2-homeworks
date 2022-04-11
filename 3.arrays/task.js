6f386b503fbb51d12bb308a9f975b7f0e9a7f1fd
VitalyBordyashov <vital.bordyashov@mail.ru>
2022-04-11 20:51:03 +0300

    исправления

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,22 +1,7 @@
 function compareArrays(arr1, arr2) {
-  let result;
-  if (arr1.length != arr2.length) {
-    result = false;
-  } else {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] === arr2[i]) {
-        result = true;
-      } else {
-        result = false;
-        break;
-      }
-    }
-  }
-  return result; // boolean
+  return arr1.length === arr2.length && arr1.every((v,i) => v === arr2[i]);
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-  resultArr  = arr.filter(elem => elem > 0).filter(elem => elem % 3 == 0).map(elem => elem * 10);
-  return resultArr; // array
+  return arr.filter(elem => elem > 0).filter(elem => elem % 3 == 0).map(elem => elem * 10);
 }
